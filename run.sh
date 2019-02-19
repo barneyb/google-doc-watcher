2fcf602be73d69292d3129000e9cbc42fb85ebfd
barneyb <bboisvert@gmail.com>
2019-02-19 13:00:46 -0800

    accept ID and PATTERN as command args, w/ PATTERN being optional

--- run.sh
+++ run.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
+#
+# I can monitor and print diffs for the plain-text export of a Google
+# Doc. Originally conceived to deal with Sherwood's theatre community's
+# inability to use calendaring software for maintaining a calendar of
+# rehearsals and performance, it ended up being more generally useful.
+# No authentication support is available, so the target document must
+# be at least publicly readable.
+#
+# The general usage pattern is to wire a cron task that invokes this
+# script w/ the document ID and an optional case-insensitive `egrep`
+# pattern which matches the subset of lines you care about. With no
+# diffs, the script is silent. If there are diffs, a unified diff will
+# be printed to STDOUT followed by a horizontal rule, and then the
+# whole document (post-filter, if filtering is enabled).
+#
+# While the script is standalone, it uses the current directory as a
+# place to cache stuff in various text files. There's no way to set
+# an alternate scratch space.
 
-ID="1t9EU4JzmTm0UEzWr0F2ZS40A-yZbwZBlFYYvkCNIle8"
-PATTERN="([23]/[0-9][0-9]?)|rally|lindsay|nun|super"
+ID="$1"
+PATTERN="$2"
+
+if [ "$ID" = "" ]; then
+	echo "Usage: `basename $0` docId [ egrepPattern ]"
+	exit 1
+fi
 
 cd `dirname $0`
 
@@ -13,7 +36,11 @@ DIFF="$ID.diff.txt"
 
 curl -s $URL > $RAW
 
-cat $RAW | egrep -i "$PATTERN" > $CURR
+if [ "$PATTERN" = "" ]; then
+	cp $RAW $CURR
+else
+	cat $RAW | egrep -i "$PATTERN" > $CURR
+fi
 
 touch $PREV # just in case
 diff -u -i -w $PREV $CURR > $DIFF
